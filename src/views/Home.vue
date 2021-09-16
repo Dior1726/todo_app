6a62db234bc77e67e4a181b6cd0f0764a6d5e8c9
Dior1726 <muxammad_909@mail.ru>
2021-09-16 07:56:06 +0600

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-center">
+      Vue ToDo
+    </h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style lang="scss" scoped>
+.text-center {
+  text-align: center;
+}
+</style>
